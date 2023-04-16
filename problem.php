1b8f7401757e6f3fd518d20f292c5ea24ed78b6b
Daniel <104028647+thuanvo121@users.noreply.github.com>
2023-04-15 23:05:26 -0300

    update problem

--- problem.php
+++ problem.php
@@ -1,14 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
     <?php
-    // include 'index.php';
     session_start();
     echo "<button onclick=\"window.location.href='logout.php';\">log out</button></br>";
 
@@ -44,5 +34,3 @@
 
     $_SESSION['emailType'] = $emailType;
     ?>
-</body>
-</html>
